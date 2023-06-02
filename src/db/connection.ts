c6694f259688c6d4d0e150811456da41c47b0639
Maxim <gumeniukmaks0510@gmail.com>
2023-06-02 23:07:47 +0300

    change connection config

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,25 +1,28 @@
-import { Sequelize } from "sequelize-typescript";
+import { Sequelize, SequelizeOptions } from "sequelize-typescript";
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 const { DB_PASS, DB_NAME, DB_HOST, DB_USER_NAME } = process.env;
 
+const sequelizeOptions: SequelizeOptions = {
+  host: DB_HOST,
+  port: 5432,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+};
+
 export const sequelize = new Sequelize(
-    DB_NAME || '', 
-    DB_USER_NAME || '', 
-    DB_PASS || '', 
-    {
-      host: DB_HOST,
-      port: 5432,
-      dialect: 'postgres',
-      dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-      },
-    }
+  DB_NAME || '',
+  DB_USER_NAME || '',
+  DB_PASS || '',
+  sequelizeOptions
 );
+
 
 sequelize.sync().then(() => {
   console.log('Database and tables created!');
