8ff6c6320dd7b9bae1eb6bcc1e61559b0000cecc
Maxim <gumeniukmaks0510@gmail.com>
2023-06-02 23:05:07 +0300

    change connection config

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -5,9 +5,9 @@ dotenv.config();
 const { DB_PASS, DB_NAME, DB_HOST, DB_USER_NAME } = process.env;
 
 export const sequelize = new Sequelize(
-    DB_NAME!, 
-    DB_USER_NAME!, 
-    DB_PASS!, 
+    DB_NAME || '', 
+    DB_USER_NAME || '', 
+    DB_PASS || '', 
     {
       host: DB_HOST,
       port: 5432,
